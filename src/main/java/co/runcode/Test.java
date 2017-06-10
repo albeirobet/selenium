16f2e25dfa439b9db9cdac5d6a4cde52475d634d
Marcela <marce1396@gmail.com>
2017-06-10 13:29:23 -0500

    Agregando funcionalidad para testNG

--- src/main/java/co/runcode/Test.java
+++ src/main/java/co/runcode/Test.java
@@ -1,5 +1,7 @@
 package co.runcode;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,16 +13,28 @@ public class Test {
 	public static void main(String[] args) {
 
 		WebDriver driver;
+		
 		ChromeDriverManager.getInstance().setup();
+		
+		
 		driver = new ChromeDriver();
 		driver.get("http://www.google.com");
 		driver.findElement(By.name("q")).sendKeys("Hola Mundo");
 		WebElement oGoogleSearchBtn = driver.findElement(By.name("btnG"));
 		oGoogleSearchBtn.click();
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+
+		List<WebElement> listElements = driver.findElements(By.className("r"));
+		if (listElements != null && listElements.size() > 0) {
+			for (WebElement webElement : listElements) {
+				System.out.println(webElement.getText());
+			}
+		}else{
+			System.out.println(" No encontre ningún link");
 		}
 
 	}
